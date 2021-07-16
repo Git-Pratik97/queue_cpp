4ff0d4e9b6fbe65ee42fbb9b591ec2693dfe3c15
Pratik Kamble <62785111+Git-Pratik97@users.noreply.github.com>
2021-07-16 20:15:45 +0530

    Add files via upload

---
+++ stack_imp_que.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+
+class que
+{
+    stack<int> s1;
+    stack<int> s2;
+
+    public:
+    void push(int x)
+    {
+        s1.push(x);
+    }
+
+    int pop()
+    {
+        if (s1.empty() && s2.empty())
+        {
+            cout << "Queue is empty\n" ;
+            return -1;
+        }
+
+        if (s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int top_val = s2.top();
+        s2.pop();
+
+        return top_val;
+    }
+
+    bool empty()
+    {
+        if(s1.empty() and s2.empty())
+        {
+            return true;
+        }
+        return false;
+    }
+
+};
+
+int main()
+{
+    que q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+
+    cout << q.pop() <<endl;
+    cout << q.pop() <<endl;
+    cout << q.pop() <<endl;
+    
+    cout << q.empty() <<endl;
+
+    q.push(5);
+
+    cout << q.pop() <<endl;
+    cout << q.pop() <<endl;
+
+    cout << q.empty() <<endl;
+
+
+    return 0;
+    
+}
